c8aba393f0f114206f864b0e0f165fc952592b6d
NumbShell <acxey32@gmail.com>
2016-10-05 00:47:10 +0200

    Add files via upload

--- weather.py
+++ weather.py
@@ -2,18 +2,6 @@ from bs4 import BeautifulSoup
 import urllib2
 import urllib
 
-
-html_doc = "<html><head><title>The machine and smoke</title></head><body><p>The functioning parts of machinery</p></body></html>"
-
-soup = BeautifulSoup('https://7273627d.dataplicity.io', 'html.parser')
-
-print(soup.prettify())
-
-t = soup.title.text
-
-print(t)
-print(soup.p)
-
 query_args = { 'RPI' : 'Active' }
 
 url = 'https://7273627d.dataplicity.io'
@@ -24,3 +12,12 @@ request = urllib2.Request(url, data)
 
 response = urllib2.urlopen(request).read()
 
+
+soup = BeautifulSoup(data, 'html.parser')
+
+print(soup.prettify())
+
+t = soup.title.text
+
+print(t)
+print(soup.p)
