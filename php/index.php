4a95d8f5be8d5407f025651fd4db3a101691d428
NumbShell <acxey32@gmail.com>
2016-10-08 18:53:33 +0200

    Files for latest project

--- php/index.php
+++ php/index.php
@@ -1,6 +1,21 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<!--<link rel="stylesheet" type="text/css" href="theme.css">-->
+</head>
+<body>
+
+<div class="forecast">
+<h1>Weather Forecast</h1>
 <?php
 	include("simple_html_dom.php");
-	$html=file_get_html("http://www.klart.se/v%C3%A4der-karlshamn.html");
-	$title=$html->find("div#city-weather",0)->innertext;
-	echo $title;
+	$html=file_get_html("https://www.imdb.com/chart/moviemeter?ref_=nv_mv_mpm_8");
+	$title=$html->find("tr.weather-icons",0)->innertext;
+
+	$cur = $html->find("ul.chat-lines",0)->innertext;
+	//$naw = $cur->find("div#current-conditions",0)->innertext;
+	echo $html;
 ?>
+</div>
+</body>
+</html>
